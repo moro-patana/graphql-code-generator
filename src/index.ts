783eb18b553f6cf8b8b2e9fd579f87d8dcb411ba
manjaka <manjaka.ran@onja.org>
2022-01-20 11:50:13 +0300

    adds packages

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,42 @@ const students: Student[] = [
   {
     firstName: "Anita",
     lastName: "Rasoa",
-    id: 1
+    id: 1,
+    imageUrl: 'https://i.ibb.co/6vhZKy4/image-1.png',
+    role: 'Front-end Developer',
+    linkedinUrl: '#',
+    twitterUrl: '#'
   },
   {
     firstName: "Noeline",
     lastName: "Marie Jeanne",
-    id: 2
+    id: 2,
+    imageUrl: 'https://i.ibb.co/q5X4bZf/image-2.png',
+    role: 'Front-end Developer',
+    linkedinUrl: '#',
+    twitterUrl: '#'
   },
   {
     firstName: "Sarah",
     lastName: "Razanatsoa",
-    id: 3
+    id: 3,
+    imageUrl: 'https://i.ibb.co/LRP5n6k/image-4.png',
+    role: 'Front-end Developer',
+    linkedinUrl: '#',
+    twitterUrl: '#'
   },
   {
     firstName: "Tantely",
     lastName: "Andrianarivola",
-    id: 4
+    id: 4,
+    imageUrl: 'https://i.ibb.co/TR9LL3q/image-3.png',
+    role: 'Front-end Developer',
+    linkedinUrl: '#',
+    twitterUrl: '#'
   },
   
 ];
+
 
 const schema = loadSchemaSync("**/*.graphql", {
     loaders: [new GraphQLFileLoader()],
